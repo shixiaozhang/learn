58a850211672db5211ccb9128b6e8e87ead0edfb
SKY-20190808QWN\Administrator <813155893@qq.com>
2020-11-26 17:48:30 +0800

    1

--- Proxy.js
+++ Proxy.js
@@ -44,33 +44,10 @@
 
 	// 读取一个不存在的属性  直接会抛出异常
     // console.log(proxy.nme);
-    let a=1;
-    function aa(){
-        console.log(a);
-       
-    }
-   
-    function bb(){
-        let a=2
-     
-       console.log(this)
-        aa.apply(this)
-    }
-   
-    bb()//1
-    a=3
-    bb()//3  函数执行会优先找词法环境，没有在找执行环境
 
-    
-    function as(){
-        console.log(1)
-        console.log(b())
 
-    }
-    as()
 
-    var b=function (){
-        return 2
-    }
+
+
 
   
