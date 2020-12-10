64fd58b99c43bf59253e66d8c7a6ab1a502548fa
zhangdafu12 <48646921+zhangdafu12@users.noreply.github.com>
2020-12-10 22:10:53 +0800

    Create apply_call_bind.js

---
+++ Other/apply_call_bind.js
@@ -0,0 +1,12 @@
+// apply call bind
+/**
+ * apply call bind区别
+ * 
+ * 
+ */
+// 1、 apply(this,[ ]) call(this,1,2,3) bind(this,1,2,3)
+// 2、 apply 与 call 传入this是修改原函数的this；
+//     bind是创建新的副本，与原函数行为一致；******
+
+
+
