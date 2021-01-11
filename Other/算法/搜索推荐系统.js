e9d0aea7101875e019dbb51cbf4858df4ebacd19
zhangdafu12 <48646921+zhangdafu12@users.noreply.github.com>
2021-01-11 23:20:33 +0800

    1

--- Other/算法/搜索推荐系统.js
+++ Other/算法/搜索推荐系统.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-01-11 17:26:34
- * @LastEditTime: 2021-01-11 17:58:12
+ * @LastEditTime: 2021-01-11 20:38:45
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \learn\Other\算法\搜索推荐系统.js
@@ -12,30 +12,32 @@
  * @return {string[][]}
  */
 var suggestedProducts = function (products, searchWord) {
-    let length = searchWord.length
     let strArr = searchWord.split('')
-    console.log(strArr);
-    
     let newStr = strArr.reduce((a, b) => {
         return `(${a})${b}`
     })
+    // products = products.sort()
     newStr = `(${newStr})`
-
     let reg = new RegExp(newStr)
     let n = searchWord.match(reg)
-    console.log(n)
-    let Nproducts= products.map(item=>{
-        item.split('').map(val=>{
-            strArr.map((value,index)=>{
-                value===val[index]
-            })
+    n=n.sort()
+    n.pop()
+    return n.map(item => {
+        let reg = new RegExp('^'+item)
+        let index=0
+        let list = products.filter(val => {
+       
+            if(index===3){
+                return false
+            }
+            if(reg.test(val)){
+                index++
+                return reg.test(val)
+            }
+            
         })
+        return list
     })
-        
-
-    return products = products.sort()
-
-
 
 };
 console.log(suggestedProducts(["mobile", "mouse", "moneypot", "monitor", "mousepad"], "mouse"));
