f5cdb0dfe997c9ed03a121e21e409be148625c85
zhangdafu12 <48646921+zhangdafu12@users.noreply.github.com>
2021-02-27 21:28:58 +0800

    1

--- Other/算法/冒泡排序.js
+++ Other/算法/冒泡排序.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-01-21 11:04:23
- * @LastEditTime: 2021-01-21 11:31:44
+ * @LastEditTime: 2021-02-27 18:59:28
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \learn\Other\算法\冒泡排序.js
@@ -15,7 +15,9 @@ function babbleSort(arr) {
         // i 循环只是为了提供 循环 次数； 
         // 为 arr 中的每一个 值进行一次 比对； 
         // 当 只剩最后一个值时 ；i 就不用循环了，因为这个值必定是最小的；
-        for (let j = 0; j < len - i - 1; j++) {// i 没循环一次，j 就完成一位的排序； 所以每次 i 循环完 j 需要遍历的长度就 减  一 。
+        for (let j = 0; j < len - i - 1; j++) {
+            // i 没循环一次，j 就完成一位的排序； 
+            // 所以每次 i 循环完 j 需要遍历的长度就 减  一 。
             if (arr[j] > arr[j + 1]) {
                 //这样会把最大的不停的与比他小的换位置，最后把最大的换到最后
                 // 在每次 i 循环结束
--- Other/算法/回文数.js
+++ Other/算法/回文数.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-01-15 17:40:56
- * @LastEditTime: 2021-01-15 17:45:21
+ * @LastEditTime: 2021-02-27 21:09:14
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \learn\Other\算法\回文数.js
@@ -14,7 +14,33 @@ var isPalindrome = function (x) {
     let list = x.toString().split('')
     list = list.reverse().toString()
     list = list.replace(/,/g, '')
-    
-    return list===x.toString()
+
+    return list === x.toString()
 };
 console.log(isPalindrome(-121));
+
+Promise._race = promises => new Promise((resolve, reject) => {
+    promises.forEach(promise => {
+        promise.then(resolve, reject)
+    })
+})
+
+Promise.myrace = function (iterator) {
+    return new Promise((resolve, reject) => {
+        try {
+            let it = iterator[Symbol.iterator]();
+            while (true) {
+                let res = it.next();
+                console.log(res);
+                if (res.done) break;
+                if (res.value instanceof Promise) {
+                    res.value.then(resolve, reject);
+                } else {
+                    resolve(res.value)
+                }
+            }
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
