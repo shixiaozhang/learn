b22c834c06cc9cd642b41d43600630d1518f2d86
zhangdafu12 <48646921+zhangdafu12@users.noreply.github.com>
2021-05-30 12:09:08 +0800

    1

--- WebpackDome/webpack-vue/src/App.vue
+++ WebpackDome/webpack-vue/src/App.vue
@@ -1,23 +1,23 @@
 <!--
  * @Author: your name
  * @Date: 2021-01-11 10:14:14
- * @LastEditTime: 2021-04-12 13:57:58
+ * @LastEditTime: 2021-05-30 11:42:49
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \learn\WebpackDome\webpack-vue\src\App.vue
 -->
 <template>
-  <div >
-      <h1 class="example">{{ msg }}</h1>
-  <img src="./assets/616945.png" alt="" srcset="">
+  <div>
+    <h1 class="example">{{ msg }}</h1>
+    <img src="./assets/616945.png" alt srcset />
   </div>
 </template>
 
 <script>
 import Axios from './api'
 export default {
-    name: "app",
-  data () {
+  name: "app",
+  data() {
     return {
       msg: 'Hello world!'
     }
@@ -25,7 +25,7 @@ export default {
   mounted() {
     Axios.get('/api')
   },
-  
+
 }
 </script>
 
