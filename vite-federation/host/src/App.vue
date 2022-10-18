e77b01d04ec3c73057390bc0d6ad3cac043ffbab
xiaozhang <813155893@qq.com>
2022-10-18 21:20:18 +0800

    vite；联邦模块

---
+++ vite-federation/host/src/App.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from "./components/HelloWorld.vue";
+import { defineAsyncComponent } from "vue";
+// 导入远程模块
+// 1. 组件
+import RemoteApp from "remote_app/App";
+// 2. 工具函数
+import { add } from "remote_app/utils";
+// 3. 异步组件
+const AysncRemoteButton = defineAsyncComponent(
+  () => import("remote_app/Button")
+);
+const data: number = add(1, 2);
+</script>
+
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/vue.svg" />
+    <HelloWorld />
+    <RemoteApp />
+    <AysncRemoteButton />
+    <p>应用 2 工具函数计算结果: 1 + 2 = {{ data }}</p>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
